37633fc68dc54da1c114b8503faa3ca570b59ae6
Thiago Lourenço <tlcb@ecomp.poli.br>
2025-07-17 14:44:32 -0300

    Create menu bar component

---
+++ src/app/features/landing/components/menu-bar/menu-bar.component.ts
@@ -0,0 +1,35 @@
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
+
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-menu-bar',
+  imports: [MenubarModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './menu-bar.component.html',
+  styleUrl: './menu-bar.component.scss',
+})
+export class MenuBarComponent implements OnInit {
+  public items: MenuItem[] = [];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+      },
+      {
+        label: 'Privacy',
+      },
+      {
+        label: 'Terms',
+      },
+      {
+        label: 'Log in',
+      },
+      {
+        label: 'Sign up',
+      },
+    ];
+  }
+}
