e6945d8f5399ece18e82e3300703c3aeeb9a6392
Thiago Lourenço <tlcb@ecomp.poli.br>
2025-07-17 14:46:31 -0300

    Remove unused dark theme config

--- src/theme.ts
+++ src/theme.ts
@@ -20,22 +20,6 @@ export const StyleTheme = definePreset(Aura, {
           950: '{zinc.950}',
         },
       },
-      dark: {
-        surface: {
-          0: '#ffffff',
-          50: '{slate.50}',
-          100: '{slate.100}',
-          200: '{slate.200}',
-          300: '{slate.300}',
-          400: '{slate.400}',
-          500: '{slate.500}',
-          600: '{slate.600}',
-          700: '{slate.700}',
-          800: '{slate.800}',
-          900: '{slate.900}',
-          950: '{slate.950}',
-        },
-      },
     },
   },
   components: {
