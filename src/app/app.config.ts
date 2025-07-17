a0f974f5c8b209673b378fd65f375f322a73ab55
Thiago Lourenço <tlcb@ecomp.poli.br>
2025-07-17 14:45:42 -0300

    Add theme in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,24 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+  ApplicationConfig,
+} from '@angular/core';
+
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
+import { StyleTheme } from '../theme';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: StyleTheme,
+      },
+    }),
+  ],
 };
