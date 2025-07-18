9ac8e872215de324283032e0c91afbe2fcae91b5
Thiago Lourenço <tlcb@ecomp.poli.br>
2025-07-18 01:17:10 -0300

    Create Theme config

--- src/theme.ts
+++ src/theme.ts
@@ -3,27 +3,30 @@ import Aura from '@primeng/themes/aura';
 
 export const StyleTheme = definePreset(Aura, {
   semantic: {
-    colorScheme: {
-      light: {
-        surface: {
-          0: '#ffffff',
-          50: '{zinc.50}',
-          100: '{zinc.100}',
-          200: '{zinc.200}',
-          300: '{zinc.300}',
-          400: '{zinc.400}',
-          500: '{zinc.500}',
-          600: '{zinc.600}',
-          700: '{zinc.700}',
-          800: '{zinc.800}',
-          900: '{zinc.900}',
-          950: '{zinc.950}',
-        },
-      },
+    primary: {
+      50: '#FCF4F2',
+      100: '#F3D6CD',
+      200: '#EBBCAD',
+      300: '#e9ab96ff',
+      400: '#e9987dff',
+      500: '#e78666ff',
+      600: '#c9775eff',
+      700: '#c9684bff',
+      800: '#D1613E',
+      900: '#220a03ff',
+      950: '#000000ff',
     },
   },
   components: {
     menubar: {
+      colorScheme: {
+        light: {
+          root: {
+            padding: '1rem 2rem',
+            background: '#FAF8F5',
+          },
+        },
+      },
       css: () => `
       .p-menubar {
         border: none;
@@ -35,14 +38,45 @@ export const StyleTheme = definePreset(Aura, {
       }
       `,
     },
-    card: {
-      root: {
-        borderRadius: '4px',
-      },
+    button: {
       colorScheme: {
         light: {
           root: {
-            background: '{indigo.400}',
+            primary: {
+              background: '{primary.color}',
+              hoverBackground: '{primary.hover.color}',
+              activeBackground: '{primary.active.color}',
+              borderColor: '{primary.color}',
+              hoverBorderColor: '{primary.hover.color}',
+              activeBorderColor: '{primary.active.color}',
+              color: '{primary.contrast.color}',
+              hoverColor: '{primary.contrast.color}',
+              activeColor: '{primary.contrast.color}',
+              focusRing: {
+                color: '{primary.color}',
+                shadow: 'none',
+              },
+            },
+            secondary: {
+              background: '{primary.900}',
+              hoverBackground: '{primary.950}',
+              borderColor: '{primary.900}',
+              activeBackground: '{primary.950}',
+              hoverBorderColor: '{primary.950}',
+              activeBorderColor: '{primary.950}',
+              color: '{primary.contrast.color}',
+              hoverColor: '{primary.contrast.color}',
+              activeColor: '{primary.contrast.color}',
+              focusRing: {
+                color: '{primary.900}',
+                shadow: 'none',
+              },
+            },
+          },
+          link: {
+            color: '{primary.900}',
+            hoverColor: '{primary.900}',
+            activeColor: '{primary.900}',
           },
         },
       },
