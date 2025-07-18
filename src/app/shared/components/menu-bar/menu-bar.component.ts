45517bb2936e18b1c1e9eaf3774fa97a360faade
Thiago Lourenço <tlcb@ecomp.poli.br>
2025-07-18 01:18:28 -0300

    Change component folder

--- src/app/shared/components/menu-bar/menu-bar.component.ts
+++ src/app/shared/components/menu-bar/menu-bar.component.ts
@@ -1,11 +1,13 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-menu-bar',
-  imports: [MenubarModule],
+  imports: [MenubarModule, CommonModule, ButtonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.scss',
